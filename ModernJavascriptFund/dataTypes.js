41dc1cc490641a493adc9f8473260985de7bd453
Jesus <jesusmontoya1992@yahoo.com>
2024-05-05 10:46:25 -0500

    Added dataTypes.js file

---
+++ ModernJavascriptFund/dataTypes.js
@@ -0,0 +1,22 @@
+let message = "hello";
+message = 123456;
+// alert(message);
+
+let n = 123;
+n = 12.35 // floating point number
+
+// infinity
+// alert(1/0);
+// alert(Infinity);
+
+// Nan = computational error
+// alert("not a number" / 2);
+// alert(NaN + 1);
+// console.log(9007199254740991 + 1); 
+
+// Strings
+let str = "hello";
+let str2 = 'single quotes are ok too';
+let phrase = `can embed another ${str}`;
+alert(phrase);
+alert(`the result is ${1+2}`);
