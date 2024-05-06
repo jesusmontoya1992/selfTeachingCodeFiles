149cd04f61d2609237dc908c97bb3744aef8ad64
Jesus <jesusmontoya1992@yahoo.com>
2024-05-06 16:16:47 -0500

    completed dataTypes.js file

--- ModernJavascriptFund/dataTypes.js
+++ ModernJavascriptFund/dataTypes.js
@@ -18,5 +18,50 @@ n = 12.35 // floating point number
 let str = "hello";
 let str2 = 'single quotes are ok too';
 let phrase = `can embed another ${str}`;
-alert(phrase);
-alert(`the result is ${1+2}`);
+// alert(phrase);
+// alert(`the result is ${1+2}`);
+
+// Boolean
+let nameFieldChecked = true;
+let ageFieldChecked = false;
+
+let isGreater = 4 > 1;
+// alert(isGreater);
+
+// null = nothing, empty, value unknown
+// let age = null;
+
+// undefined = value not assigned
+// let undefinedAge;
+// alert(undefinedAge);
+
+// let age = 100;
+// age = undefined;
+// alert(age);
+
+// typeof operator
+console.log(typeof undefined);
+console.log(typeof 0);
+console.log(typeof 10n);
+console.log(typeof true);
+console.log(typeof "foo");
+console.log(typeof Symbol("id"));
+console.log(typeof Math);
+console.log(typeof null);
+console.log(typeof alert);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
