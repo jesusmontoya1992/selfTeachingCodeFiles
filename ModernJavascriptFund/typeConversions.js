321a8176e091f7454fcf7a42adbaaeb6256f7a7d
Jesus <jesusmontoya1992@yahoo.com>
2024-05-07 09:16:34 -0500

    completed typeConversions.js file

--- ModernJavascriptFund/typeConversions.js
+++ ModernJavascriptFund/typeConversions.js
@@ -8,4 +8,27 @@
 // alert(typeof value);
 
 // Numeric coversion
-alert("6" / '2'); 
+// alert("6" / '2');  /*it coverts automically to numbers*/
+
+// explicitly convert
+// let str = "123";
+// alert(typeof str);
+// let num = Number(str);
+// alert(typeof num);
+
+// No number = NaN
+// let age = Number("not a number");
+// alert(age);
+
+// true = 1 false = 0
+
+// Boolean Coversion
+alert(Boolean(1));
+alert(Boolean(0));
+alert(Boolean("hello"));
+alert(Boolean(""));
+
+
+
+
+
