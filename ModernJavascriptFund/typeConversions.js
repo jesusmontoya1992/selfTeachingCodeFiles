334e58ed00fb25d3868c3b9d88041dbea141e385
Jesus <jesusmontoya1992@yahoo.com>
2024-05-06 23:24:34 -0500

    working on typeConversion.js file

---
+++ ModernJavascriptFund/typeConversions.js
@@ -0,0 +1,11 @@
+"use strict";
+
+// string conversion
+// let value = true;
+// alert(typeof value);
+
+// value = String(value);
+// alert(typeof value);
+
+// Numeric coversion
+alert("6" / '2'); 
