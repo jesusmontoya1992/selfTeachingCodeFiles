dda78cbd2c0a705022d83f804c7a258527a069e8
Jesus <jesusmontoya1992@yahoo.com>
2024-05-06 22:17:53 -0500

    Added and completed alertPromptConfirm.js file

---
+++ ModernJavascriptFund/alertPromptConfirm.js
@@ -0,0 +1,13 @@
+"use strict";
+// alert("hello");
+
+// result = prompt(title, [optional]);
+
+// let age = prompt('how old are you?', 100);
+// alert(`You are ${age} years old`);
+
+// let isBoss = confirm("Are you the boss?");
+// alert(isBoss);
+
+let name = prompt('what is your name?', ' ');
+alert(`Your name is ${name}`);
