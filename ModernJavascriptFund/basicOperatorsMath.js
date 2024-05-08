ea270ef500e42e7ab807024acd2044369af55ba8
Jesus <jesusmontoya1992@yahoo.com>
2024-05-07 19:22:18 -0500

    added to basicOperatorsMath.js file

---
+++ ModernJavascriptFund/basicOperatorsMath.js
@@ -0,0 +1,45 @@
+"use strict"
+// let x = 1;
+// x = -x;
+// alert(x);
+
+// let x = 1, y = 3;
+// alert(y - x);
+
+// remainder operator %
+// alert(5 % 2);
+// alert(8 % 3);
+// alert(8 % 4);
+
+// exponentation **
+// alert(2 ** 2);
+// alert(2 ** 3);
+// alert(2 ** 4);
+
+// concatenation
+// let s = "my" + " string";
+// alert(s);
+// alert('1' + 2);
+// alert(2 + "1");
+// alert(2 + 2 + '1');
+// alert('1' + 2 + 2);
+
+// substraction and division
+// alert(6 - "2");
+// alert("6" / 2);
+
+// unary +
+// let x = 1;
+// alert(+x);
+
+// let y = -2;
+// alert(+y);
+
+// alert(+true);
+// alert(+"");
+
+let apples = "2";
+let oranges = "3";
+alert(+apples + +oranges);
+
+
