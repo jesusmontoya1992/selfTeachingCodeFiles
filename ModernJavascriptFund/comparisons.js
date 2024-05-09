0455f8aaa84b61f43787b595e04b7678416500cd
Jesus <jesusmontoya1992@yahoo.com>
2024-05-09 10:02:55 -0500

    created comparison.js

---
+++ ModernJavascriptFund/comparisons.js
@@ -0,0 +1,16 @@
+"use strict"
+// all comparison operators return boolean values
+// alert (2 > 1);
+// alert (2 == 1);
+// alert (2 != 1);
+
+// let result = 5 > 4;
+// alert(result);
+
+alert('z' > 'a');
+alert('a' > 'Z'); /*lowercase is greater than uppercase*/
+
+// comparison of different types
+
+
+
