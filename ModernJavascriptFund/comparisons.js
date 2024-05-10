f289552d79582baf7aefeb883135ea4be7bf5f24
Jesus <jesusmontoya1992@yahoo.com>
2024-05-10 18:58:17 -0500

    Completed comparisons.js

--- ModernJavascriptFund/comparisons.js
+++ ModernJavascriptFund/comparisons.js
@@ -7,10 +7,26 @@
 // let result = 5 > 4;
 // alert(result);
 
-alert('z' > 'a');
-alert('a' > 'Z'); /*lowercase is greater than uppercase*/
+// alert('z' > 'a');
+// alert('a' > 'Z'); /*lowercase is greater than uppercase*/
 
 // comparison of different types
+// values are converted into numbers
+// alert('2' > 1);
+// alert('01' == 1);
+
+// alert(true == 1);
+// alert(false == 0);  /* true = 1, false = 0 */
+
+// strict equality
+// alert (0 == false);
+// alert (' ' == false);
+
+alert( 0 === false);
+alert (null == undefined); /* null becomes 0 & undefined = NaN*/
+
+
+
 
 
 
