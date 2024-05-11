e45718e032b8e25599514269420dcbcefaebfaab
Jesus <jesusmontoya1992@yahoo.com>
2024-05-11 09:26:52 -0500

    created if.js

---
+++ ModernJavascriptFund/if.js
@@ -0,0 +1,18 @@
+"use strict"
+// let year = prompt('In which year was ECMAScript-2015 published?', '');
+// // if (year == 2015) alert('You are right!');
+// if (year == 2015) {
+//     alert("That's correct");
+//     alert('You are so smart');
+// }
+
+// else clause
+let year = prompt('In which year was ECMAScript-2015 published?', '');
+if (year == 2015) {
+    alert("You guessed it right!");
+} else {
+    alert('How can you be so wrong?');
+}
+
+// several else if
+
