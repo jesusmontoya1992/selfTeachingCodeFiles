b84efa3cf58c73ddd195e3f1d321620f27b00cf8
Jesus <jesusmontoya1992@yahoo.com>
2024-05-12 20:03:41 -0500

    Created logicalOperators.js

---
+++ ModernJavascriptFund/logicalOperators.js
@@ -0,0 +1,37 @@
+"use strict"
+// || = or
+// result = a || b;
+
+// if (1 || 0) {
+//     alert("truthy!");
+// }
+
+// let hour = 9;
+// if (hour < 10 || hour > 18) {
+//     alert('The office is closed.');
+// }
+// pass more conditions
+// let hour = 12;
+// let isWeekend = true;
+
+// if (hour < 10 || hour > 18 || isWeekend) {
+//     alert('The office is closed.');
+// }
+
+// OR || finds the first thruthy value
+// alert (1 || 0);
+// alert(null || 1);
+// alert(null || 0 || 1);
+// alert(undefined || null || 0);
+
+// let firstName = '';
+// let lastName = "";
+// let nickName = "SuperCoder";
+// alert(firstName || lastName || nickName || "Anonymous");
+
+true || alert("Not printed");
+false || alert("printed");
+
+// && operator
+
+
