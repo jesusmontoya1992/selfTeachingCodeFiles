e8cc8176d64ecdbcee4baf24f87912ae4a1971e2
Jesus <jesusmontoya1992@yahoo.com>
2024-05-14 15:54:52 -0500

    created loops.js

--- ModernJavascriptFund/nullishOperator.js
+++ ModernJavascriptFund/nullishOperator.js
@@ -12,3 +12,17 @@
 // alert(firstName ?? lastName ?? nickName ?? "Anonymous");
 
 // comparison with ||
+// let firstName = null;
+// let lastName = null;
+// let nickName = "SuperCoder";
+// alert(firstName || lastName || nickName || "Anonymous");
+
+// let height = 0;
+// alert(height || 100);
+// alert(height ?? 100);
+
+// Precedence
+let height = null;
+let width = null;
+let area = (height ?? 100) * (width ?? 50);
+alert(area);
