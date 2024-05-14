bd811be0d7ca5ca221e640a5a74d083a09b15524
Jesus <jesusmontoya1992@yahoo.com>
2024-05-14 08:53:59 -0500

    created nullishOperator.js

---
+++ ModernJavascriptFund/nullishOperator.js
@@ -0,0 +1,14 @@
+"use strict"
+// ?? = returns first defined value
+// let user;
+// alert(user ?? "Anonymous");
+
+// let user = "John";
+// alert(user ?? "anonymous");
+
+// let firstName = null;
+// let lastName = null;
+// let nickName = "supercoder";
+// alert(firstName ?? lastName ?? nickName ?? "Anonymous");
+
+// comparison with ||
