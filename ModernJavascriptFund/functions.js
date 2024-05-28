87af24148b978bd25bae26aef1e3264339a045d2
Jesus <jesusmontoya1992@yahoo.com>
2024-05-28 13:47:20 -0500

    completed functions.js and created functionExpressions.js

--- ModernJavascriptFund/functions.js
+++ ModernJavascriptFund/functions.js
@@ -50,14 +50,74 @@
 // showMessage("Ann", "Hello!");
 // showMessage("Ann", "What's up?");
 
-function showMessage (from, text) {
-    from = "*" + from + "*";
-    alert(from + ": " + text) ;
-}
-let from = "Ann";
-showMessage(from, "Hello");
-alert(from); /* outer variable remains unchanged */
+// function showMessage (from, text) {
+//     from = "*" + from + "*";
+//     alert(from + ": " + text) ;
+// }
+// let from = "Ann";
+// showMessage(from, "Hello");
+// alert(from); /* outer variable remains unchanged */
 
 // Default values
+// function showMessage (from, text = "no text given") {
+//     alert( from + ": " + text);
+// }
+// showMessage("Ann");
+// showMessage("Ann", "hello");
+// showMessage("Ann", undefined);
+
+// alternative default parameters
+// function showMessage(text) {
+//     if (text === undefined) {
+//         text = "empty message";
+//     }
+//     alert(text);
+// }
+// showMessage();
+
+// can also be accomplished using the || operator
+// function showMessage(text) {
+//     text = text || "empty";
+//     alert(text);
+// }
+// showMessage();
+
+// function showCount(count) {
+//     alert(count ?? "unknown")
+//     // if count is undefined or null, show "unknown"
+// }
+// showCount(0);
+// showCount(null);
+// showCount();
+
+// Returning a value
+// function sum(a, b) {
+//     return a + b;
+// }
+// let result = sum(1, 2);
+// alert(result);
+
+function checkAge(age) {
+    if (age >= 18) {
+        return true;
+    } else {
+        return confirm("Do you have permission from your parents?");
+    }
+}
+let age = prompt("How old are you?", 18);
+if (checkAge(age)) {
+    alert("Access granted");
+} else {
+    alert("Access denied");
+};
+
+let movieAge = prompt("How old are you to watch the movie?", 17);
+function showMovie(movieAge) {
+    if (!checkAge(movieAge)) {
+     return alert("Showing you the movie");
+    } else {
+     return;
+    }
+};
 
 
