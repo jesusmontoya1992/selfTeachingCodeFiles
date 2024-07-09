3814b319cb63273db035de637d877503a64c3ddc
Jesus <jesusmontoya1992@yahoo.com>
2024-07-09 13:25:46 -0500

    completed arrowFunctionsRevisited.js

--- ModernJavascriptFund/arrowFunctionsRevisited.js
+++ ModernJavascriptFund/arrowFunctionsRevisited.js
@@ -1,1 +1,24 @@
 "use strict";
+// let group = {
+//     title: "Our Group",
+//     students: ["John", "Pete", "Alice"],
+//     showList () {
+//         this.students.forEach (
+//             student => alert(this.title + ": " + student)
+//         );
+//     }
+// };
+// group.showList();
+
+// arrow have no arguments
+function defer (f, ms) {
+    return function () {
+        setTimeout( () => f.apply(this, arguments), ms);
+    };
+}
+function sayHi (who) {
+    alert("Hello, " + who);
+}
+let sayHiDeffered = defer(sayHi, 2000);
+sayHiDeffered("John");
+
